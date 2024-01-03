558a62a0718c54bebbc8085b117df84c9ae281cf
grzegorzsmereczniak <smereczniaq@gmail.com>
2024-01-03 19:18:19 +0100

    Added api_client, modified git_ignore so it ignores mac os files

---
+++ api_client.py
@@ -0,0 +1,31 @@
+import requests
+
+def fetch_data(patient_id: int):
+    url = f'http://tesla.iem.pw.edu.pl:9080/v2/monitor/{patient_id}'
+
+    try:
+        response = requests.get(url)
+    except requests.ConnectionError:
+        return "Connection Error"
+
+    response_json = response.json()
+
+    return {
+        'birthdate': response_json['birthdate'],
+        'disabled': response_json['disabled'],
+        'firstname': response_json['firstname'],
+        'id': response_json['id'],
+        'lastname': response_json['lastname'],
+        'L0_value': response_json['trace']['sensors'][0]['value'],
+        'L0_anomaly': response_json['trace']['sensors'][0]['anomaly'],
+        'L1_value': response_json['trace']['sensors'][1]['value'],
+        'L1_anomaly': response_json['trace']['sensors'][1]['anomaly'],
+        'L2_value': response_json['trace']['sensors'][2]['value'],
+        'L2_anomaly': response_json['trace']['sensors'][2]['anomaly'],
+        'R0_value': response_json['trace']['sensors'][3]['value'],
+        'R0_anomaly': response_json['trace']['sensors'][3]['anomaly'],
+        'R1_value': response_json['trace']['sensors'][4]['value'],
+        'R1_anomaly': response_json['trace']['sensors'][4]['anomaly'],
+        'R2_value': response_json['trace']['sensors'][5]['value'],
+        'R2_anomaly': response_json['trace']['sensors'][5]['anomaly'],
+    }
--- app.py
+++ app.py
@@ -5,3 +5,4 @@ app.layout = html.Div("Hello Plotly Dash with Flask!")
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
