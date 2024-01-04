7327fce5675ccf04b11604908202848eb1f88745
grzegorzsmereczniak <smereczniaq@gmail.com>
2024-01-04 15:37:47 +0100

    small changes in storage.py and api_client.py, changed name from measurements_loader.py to storage.py

---
+++ storage.py
@@ -0,0 +1,88 @@
+import logging
+import time
+
+global _storage
+
+
+def init_storage():
+    global _storage
+    _storage = {}
+    return _storage
+
+
+def get_storage():
+    global _storage
+    return _storage
+
+
+def add_measurements(patient_id, data):
+    storage = get_storage()
+
+    try:
+        if patient_id not in storage:
+            patient_data = {
+                "birthdate": data["birthdate"],
+                "disabled": data["disabled"],
+                "firstname": data["firstname"],
+                "lastname": data["lastname"],
+                'traces': {
+                    "timestamps": [],
+                    "L0_values": [],
+                    "L0_anomalies": [],
+                    "L1_values": [],
+                    "L1_anomalies": [],
+                    "L2_values": [],
+                    "L2_anomalies": [],
+                    "R0_values": [],
+                    "R0_anomalies": [],
+                    "R1_values": [],
+                    "R1_anomalies": [],
+                    "R2_values": [],
+                    "R2_anomalies": [],
+                    "_expire_ts": []
+                }
+            }
+            storage[patient_id] = patient_data
+        else:
+            patient_data = storage[patient_id]
+
+        # convert timestamp
+        timestamp_str = str(data['trace']['timestamps'])
+        timestamp = f"{timestamp_str[0:-12]}:{timestamp_str[-12:-10]}:{timestamp_str[-10:-8]}"
+        patient_data['traces']['timestamps'].append(timestamp)
+        patient_data['traces']['L0_values'].append(data['trace']['L0_value'])
+        patient_data['traces']['L0_anomalies'].append(data['trace']['L0_anomaly'])
+        patient_data['traces']['L1_values'].append(data['trace']['L1_value'])
+        patient_data['traces']['L1_anomalies'].append(data['trace']['L1_anomaly'])
+        patient_data['traces']['L2_values'].append(data['trace']['L2_value'])
+        patient_data['traces']['L2_anomalies'].append(data['trace']['L2_anomaly'])
+        patient_data['traces']['R0_values'].append(data['trace']['R0_value'])
+        patient_data['traces']['R0_anomalies'].append(data['trace']['R0_anomaly'])
+        patient_data['traces']['R1_values'].append(data['trace']['R1_value'])
+        patient_data['traces']['R1_anomalies'].append(data['trace']['R1_anomaly'])
+        patient_data['traces']['R2_values'].append(data['trace']['R2_value'])
+        patient_data['traces']['R2_anomalies'].append(data['trace']['R2_anomaly'])
+        patient_data['traces']['_expire_ts'].append(time.time())
+    except TypeError:
+        logging.warning("No data! Make sure you are connected to VPN")
+
+
+def expire_data(s):
+    storage = get_storage()
+    for patient_id, patient_data in storage.items():
+        ts = time.time()
+        while len(patient_data["_expire_ts"]) > 0 and patient_data["_expire_ts"][0] < (ts - s):
+            patient_data['traces']['timestamps'].pop(0)
+            patient_data['traces']['L0_values'].pop(0)
+            patient_data['traces']['L0_anomalies'].pop(0)
+            patient_data['traces']['L1_values'].pop(0)
+            patient_data['traces']['L1_anomalies'].pop(0)
+            patient_data['traces']['L2_values'].pop(0)
+            patient_data['traces']['L2_anomalies'].pop(0)
+            patient_data['traces']['R0_values'].pop(0)
+            patient_data['traces']['R0_anomalies'].pop(0)
+            patient_data['traces']['R1_values'].pop(0)
+            patient_data['traces']['R1_anomalies'].pop(0)
+            patient_data['traces']['R2_values'].pop(0)
+            patient_data['traces']['R2_anomalies'].pop(0)
+            patient_data['traces']['_expire_ts'].pop(0)
