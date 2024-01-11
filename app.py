4d4afc18debc21050df7fb92f52716bbd4fb5d1f
stasiekq <staszek5286@gmail.com>
2024-01-11 18:27:02 +0100

    very primitive heat visualisation

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
 import plotly.graph_objs as go
 import storage
 from api_client import fetch_data
 import time
 import threading
-from plotly.subplots import make_subplots
+import plotly.express as px
 
 storage.init_storage()
 
@@ -28,13 +27,29 @@ thread.start()
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H1(children='PPDV Final Project', style={'textAlign':'center'}),
+    html.H1(children='PPDV Final Project', style={'textAlign': 'center'}),
     dcc.Dropdown(['1', '2', '3', '4', '5', '6'], '1', id='patient-dropdown'),
-    dcc.Checklist(['L0','L1','L2','R0','R1','R2'], ['L0'], id='sensor-checklist', inline=True),
-    dcc.Graph(
-        id='sensor-graph',
-        figure={}
-    ),
+    dcc.Checklist(['L0', 'L1', 'L2', 'R0', 'R1', 'R2'], ['L0'], id='sensor-checklist', inline=True),
+    html.Div([
+        dcc.Graph(
+            id='sensor-graph',
+            figure={}
+        ),
+        html.Div([
+            dcc.Graph(
+                id='l0-column-chart',
+                figure={}
+            ),
+            dcc.Graph(
+                id='l1-column-chart',
+                figure={}
+            ),
+            dcc.Graph(
+                id='l2-column-chart',
+                figure={}
+            ),
+        ], style={'display': 'flex', 'flexDirection': 'row'}),
+    ]),
     dcc.Interval(
         id='interval-component',
         interval=100,
@@ -43,8 +58,13 @@ app.layout = html.Div([
 ])
 
 @callback(
-    Output('sensor-graph', 'figure'),
-    [Input('patient-dropdown', 'value'), Input('sensor-checklist', 'value'), Input('interval-component', 'n_intervals')],
+    [Output('sensor-graph', 'figure'), 
+     Output('l0-column-chart', 'figure'), 
+     Output('l1-column-chart', 'figure'), 
+     Output('l2-column-chart', 'figure')],
+    [Input('patient-dropdown', 'value'), 
+     Input('sensor-checklist', 'value'), 
+     Input('interval-component', 'n_intervals')],
 )
 def update_output(patient_id, sensor_ids, n_intervals):
     patient_data = storage.get_storage()[int(patient_id)]
@@ -52,25 +72,88 @@ def update_output(patient_id, sensor_ids, n_intervals):
     lastname = patient_data['lastname']
     timestamps = patient_data['traces']['timestamps']
 
-    data = []
-
+    # Wykres punktowy
+    sensor_data = []
     for sensor_id in sensor_ids:
-        p = {
-            'x': patient_data['traces']['timestamps'],
+        trace = {
+            'x': timestamps,
             'y': patient_data['traces'][f'{sensor_id}_values'],
             'type': 'scatter',
             'name': f'{sensor_id} sensor',
         }
+        sensor_data.append(trace)
 
-        data.append(p)
+    # Wykres kolumnowy dla L0
+    l0_values = patient_data['traces']['L0_values']
+    l0_color_scale = px.colors.sequential.Plasma
+    l0_chart_data = [{
+        'x': ['L0'],
+        'y': [l0_values[-1]],
+        'type': 'bar',
+        'marker': {'color': [l0_color_scale[int(l0_values[-1] / 1000 * (len(l0_color_scale) - 1))]]},
+    }]
 
-
-    return {
-        'data': data,
-        'layout': {
-            'title': f'Scatter for sensors - patient {firstname} {lastname}',
-        }
+    layout = {
+        'title': f'Scatter for sensors - patient {firstname} {lastname}',
+        'height': 400,
+        'width': 600,
     }
+
+    l0_chart_layout = {
+        'title': f'L0 Column Chart - {firstname} {lastname}',
+        'barmode': 'group',
+        'height': 400,
+        'width': 600,
+    }
+    
+    # Wykres kolumnowy dla L1
+    l1_values = patient_data['traces']['L1_values']
+    l1_color_scale = px.colors.sequential.Plasma
+    l1_chart_data = [{
+        'x': ['L1'],
+        'y': [l1_values[-1]],
+        'type': 'bar',
+        'marker': {'color': [l1_color_scale[int(l1_values[-1] / 1000 * (len(l1_color_scale) - 1))]]},
+    }]
+
+    layout = {
+        'title': f'Scatter for sensors - patient {firstname} {lastname}',
+        'height': 400,
+        'width': 600,
+    }
+
+    l1_chart_layout = {
+        'title': f'L1 Column Chart - {firstname} {lastname}',
+        'barmode': 'group',
+        'height': 400,
+        'width': 600,
+    }
+    
+    # Wykres kolumnowy dla L2
+    l2_values = patient_data['traces']['L2_values']
+    l2_color_scale = px.colors.sequential.Plasma
+    l2_chart_data = [{
+        'x': ['L2'],
+        'y': [l2_values[-1]],
+        'type': 'bar',
+        'marker': {'color': [l2_color_scale[int(l2_values[-1] / 1000 * (len(l2_color_scale) - 1))]]},
+    }]
+
+    layout = {
+        'title': f'Scatter for sensors - patient {firstname} {lastname}',
+        'height': 400,
+        'width': 600,
+    }
+
+    l2_chart_layout = {
+        'title': f'L2 Column Chart - {firstname} {lastname}',
+        'barmode': 'group',
+        'height': 400,
+        'width': 600,
+    }
+    
+
+    return {'data': sensor_data, 'layout': layout}, {'data': l0_chart_data, 'layout': l0_chart_layout}, {'data': l1_chart_data, 'layout': l1_chart_layout}, {'data': l2_chart_data, 'layout': l2_chart_layout}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
