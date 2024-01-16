c6b4b3ba8697830b09c65db7ddc8e0fa83ca6cdc
stasiekq <staszek5286@gmail.com>
2024-01-16 13:07:06 +0100

    steady progress with custom component

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def collect_data():
             for i in range(1, 7):
                 data = fetch_data(i)
                 storage.add_measurements(i, data)
-            storage.expire_data(600)
+            storage.expire_data(60)
 
             time.sleep(0.2)
         except Exception as e:
@@ -41,6 +41,10 @@ app.layout = html.Div([
             id='hm-subplots',
             figure={}
         ),
+        dcc.Graph(
+            id='hm-take2',
+            figure={}
+        ),
     ]),
     dcc.Interval(
         id='interval-component',
@@ -51,7 +55,8 @@ app.layout = html.Div([
 
 @callback(
     [Output('sensor-graph', 'figure'), 
-     Output('hm-subplots', 'figure')],
+     Output('hm-subplots', 'figure'),
+     Output('hm-take2', 'figure')],
     [Input('patient-dropdown', 'value'), 
      Input('sensor-checklist', 'value'), 
      Input('interval-component', 'n_intervals')],
@@ -82,9 +87,19 @@ def update_output(patient_id, sensor_ids, n_intervals):
     }  
 
     # Wykres heatmap dla ostatniego pomiaru
-    l0_h = px.imshow(np.array([last_measurement['L0_value']]).reshape(1, 1), color_continuous_scale='Plasma')
+    l0_h = px.imshow(np.array([last_measurement['L0_value']]).reshape(1, 1), color_continuous_scale='Plasma', text_auto=True)
     l1_h = px.imshow(np.array([last_measurement['L1_value']]).reshape(1, 1), color_continuous_scale='Plasma')
     l2_h = px.imshow(np.array([last_measurement['L2_value']]).reshape(1, 1), color_continuous_scale='Plasma')
+    
+    to_display = [
+        [last_measurement['L0_value'], last_measurement['R0_value']],
+        [last_measurement['L1_value'], last_measurement['R1_value']],
+        [last_measurement['L2_value'], last_measurement['R2_value']],
+    ]
+    
+    to_display_arr = np.array(to_display)
+    
+    another_take = px.imshow(to_display_arr)
 
     # Wykres kolumnowy dla L0
     l0_values = patient_data['traces']['L0_values']
@@ -95,6 +110,8 @@ def update_output(patient_id, sensor_ids, n_intervals):
         'type': 'bar',
         'marker': {'color': [l0_color_scale[int(l0_values[-1] / 1000 * (len(l0_color_scale) - 1))]]},
     }]
+    
+    #another_take = make_subplots(rows=1, cols=3, subplot_titles=['L0 Heatmap', 'L1 Heatmap', 'L2 Heatmap'])
 
     # Połączenie heatmap w jeden subplot
     fig_heatmaps = make_subplots(rows=1, cols=3, subplot_titles=['L0 Heatmap', 'L1 Heatmap', 'L2 Heatmap'])
@@ -103,7 +120,7 @@ def update_output(patient_id, sensor_ids, n_intervals):
     fig_heatmaps.add_trace(l2_h['data'][0], row=1, col=3)
     fig_heatmaps.update_layout(height=400, width=1200, title_text=f'Heatmaps - {firstname} {lastname}')
 
-    return {'data': sensor_data, 'layout': layout}, fig_heatmaps
+    return {'data': sensor_data, 'layout': layout}, fig_heatmaps, another_take
 
 
 if __name__ == '__main__':
